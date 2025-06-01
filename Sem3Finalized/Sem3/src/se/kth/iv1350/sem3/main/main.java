de98b83d4340c059b2bd8d90d46f198ca6579716
Jacob Polanco Johansson <88554260+Jacob-PJ@users.noreply.github.com>
2025-06-02 01:55:35 +0200

    Delete Sem3Finalized/Sem3 directory

--- Sem3Finalized/Sem3/src/se/kth/iv1350/sem3/main/main.java
+++
@@ -1,8 +0,0 @@
-package se.kth.iv1350.sem3.main;
-
-public class main {
-    public static void main(String[] args) {
-        // Start the application
-        se.kth.iv1350.sem3.startUp.StartUp.startUp(args);
-    }
-}
